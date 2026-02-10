4a326332e989f1900f483119b4a0ab06d576dc91
MW-0317 <mark.williams1703@gmail.com>
2026-02-09 23:25:18 -0500

    bleh

--- addons/mikroutils/Scripts/PlaceholderTexture.gd
+++ addons/mikroutils/Scripts/PlaceholderTexture.gd
@@ -2,6 +2,8 @@
 extends MeshInstance3D
 
 var visual_shader : VisualShader
+var number_of_parameters : int = 0
+var properties = []
 
 # cant add dynamic descriptions until this is pushed in godot: https://github.com/godotengine/godot/pull/115182
 
@@ -9,123 +11,54 @@ func _enter_tree() -> void:
 	visual_shader = (get_active_material(0) as ShaderMaterial).shader as VisualShader
 
 func property_item(parameter : VisualShaderNodeParameter) -> Dictionary:
-	var shader_parameters : Array[ShaderParameter] = [
-		# Boolean
-		ShaderParameter.new(
-			TYPE_BOOL,
-			VisualShaderNodeBooleanParameter,
-			0,
-			""
-		),
-		# Int
-		ShaderParameter.new(
-			TYPE_INT,
-			VisualShaderNodeIntParameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_INT,
-			VisualShaderNodeUIntParameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_FLOAT,
-			VisualShaderNodeFloatParameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_VECTOR2,
-			VisualShaderNodeVec2Parameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_VECTOR3,
-			VisualShaderNodeVec3Parameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_VECTOR4,
-			VisualShaderNodeVec4Parameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_COLOR,
-			VisualShaderNodeColorParameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_TRANSFORM3D,
-			VisualShaderNodeTransformParameter,
-			0,
-			""
-		),
-		ShaderParameter.new(
-			TYPE_OBJECT,
-			VisualShaderNodeTexture2DParameter,
-			PROPERTY_HINT_RESOURCE_TYPE,
-			"Texture2D"
-		),
-		ShaderParameter.new(
-			TYPE_OBJECT,
-			VisualShaderNodeTexture3DParameter,
-			PROPERTY_HINT_RESOURCE_TYPE,
-			"Texture3D"
-		),
-		ShaderParameter.new(
-			TYPE_OBJECT,
-			VisualShaderNodeTexture2DArrayParameter,
-			PROPERTY_HINT_RESOURCE_TYPE,
-			"Texture2DArray"
-		),
-		ShaderParameter.new(
-			TYPE_OBJECT,
-			VisualShaderNodeCubemapParameter,
-			PROPERTY_HINT_RESOURCE_TYPE,
-			"Cubemap"
-		),
-	]
-	
 	var hint : PropertyHint
 	var index = 0
-	for i in range(shader_parameters.size()):
-		var index_parameter : ShaderParameter = shader_parameters[i]
+	for i in range(ShaderParameter.SHADER_PARAMETERS.size()):
+		var index_parameter : ShaderParameter = ShaderParameter.SHADER_PARAMETERS[i]
 		if is_instance_of(parameter, index_parameter.TYPE):
 			index = i
 			break
 
-	print("PlaceHolderTexture.gd // property_item() // i, type, base type, hint: ", 
-	index, " ", shader_parameters[index].TYPE, " ", shader_parameters[index].BASE_TYPE, " ", shader_parameters[index].HINT)
+	#print("PlaceHolderTexture.gd // property_item() // i, type, base type, hint: ", 
+	#index, " ", ShaderParameter.SHADER_PARAMETERS[index].TYPE, " ", ShaderParameter.SHADER_PARAMETERS[index].BASE_TYPE, " ", ShaderParameter.SHADER_PARAMETERS[index].HINT)
 	return {
 		"name" : parameter.parameter_name,
-		"type" : shader_parameters[index].BASE_TYPE,
-		"hint" : shader_parameters[index].HINT,
-		"hint_string" : shader_parameters[index].HINT_STRING
+		"type" : ShaderParameter.SHADER_PARAMETERS[index].BASE_TYPE,
+		"hint" : ShaderParameter.SHADER_PARAMETERS[index].HINT,
+		"hint_string" : ShaderParameter.SHADER_PARAMETERS[index].HINT_STRING
 	}
 
 func _get_property_list():
+	ShaderParameter.init_consts()
 	var node_parameters : Array[VisualShaderNodeParameter] = []
-
-
 	for i in range(VisualShader.TYPE_MAX):
 		var node_list := visual_shader.get_node_list(i)
 		for node_id in node_list:
-			print("PlaceholderTexture.gd // _ready() // Getting node ", node_id, " of ", i)
+			#print("PlaceholderTexture.gd // _ready() // Getting node ", node_id, " of ", i)
 			var node := visual_shader.get_node(i, node_id)
 			if node is VisualShaderNodeParameter:
 				var parameter_node : VisualShaderNodeParameter = node
 				node_parameters.append(parameter_node)
+	number_of_parameters = node_parameters.size()
 
-	var properties = []
+	properties = []
 
 	for parameter in node_parameters:
+		#(get_active_material(0) as ShaderMaterial).set_shader_parameter(parameter.parameter_name, )
 		properties.append(property_item(parameter))
 
 	return properties
-	
+
+func _get(property):
+	for placeholder_property : Dictionary in properties:
+		if (placeholder_property["name"] == property):
+			return (get_active_material(0) as ShaderMaterial).get_shader_parameter(property)
+	#print("PlaceHolderTexture.gd // _get() // ", property)
+	#return (get_active_material(0) as ShaderMaterial).get_shader_parameter(parameter.parameter_name)
+
+func _set(property, value):
+	for placeholder_property : Dictionary in properties:
+		if (placeholder_property["name"] == property):
+			(get_active_material(0) as ShaderMaterial).set_shader_parameter(property, value)
+			return true
+	return false
--- addons/mikroutils/Scripts/ShaderParameter.gd
+++ addons/mikroutils/Scripts/ShaderParameter.gd
@@ -6,8 +6,94 @@ var TYPE
 var HINT : int
 var HINT_STRING : String
 
+static var SHADER_PARAMETERS : Array[ShaderParameter]
+
 func _init(p_BASE_TYPE, p_TYPE, p_HINT : int, p_HINT_STRING : String):
 	BASE_TYPE = p_BASE_TYPE
 	TYPE = p_TYPE
 	HINT = p_HINT
 	HINT_STRING = p_HINT_STRING
+	
+static func init_consts():
+	SHADER_PARAMETERS = [
+		# Boolean
+		ShaderParameter.new(
+			TYPE_BOOL,
+			VisualShaderNodeBooleanParameter,
+			0,
+			""
+		),
+		# Int
+		ShaderParameter.new(
+			TYPE_INT,
+			VisualShaderNodeIntParameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_INT,
+			VisualShaderNodeUIntParameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_FLOAT,
+			VisualShaderNodeFloatParameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_VECTOR2,
+			VisualShaderNodeVec2Parameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_VECTOR3,
+			VisualShaderNodeVec3Parameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_VECTOR4,
+			VisualShaderNodeVec4Parameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_COLOR,
+			VisualShaderNodeColorParameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_TRANSFORM3D,
+			VisualShaderNodeTransformParameter,
+			0,
+			""
+		),
+		ShaderParameter.new(
+			TYPE_OBJECT,
+			VisualShaderNodeTexture2DParameter,
+			PROPERTY_HINT_RESOURCE_TYPE,
+			"Texture2D"
+		),
+		ShaderParameter.new(
+			TYPE_OBJECT,
+			VisualShaderNodeTexture3DParameter,
+			PROPERTY_HINT_RESOURCE_TYPE,
+			"Texture3D"
+		),
+		ShaderParameter.new(
+			TYPE_OBJECT,
+			VisualShaderNodeTexture2DArrayParameter,
+			PROPERTY_HINT_RESOURCE_TYPE,
+			"Texture2DArray"
+		),
+		ShaderParameter.new(
+			TYPE_OBJECT,
+			VisualShaderNodeCubemapParameter,
+			PROPERTY_HINT_RESOURCE_TYPE,
+			"Cubemap"
+		),
+	]
